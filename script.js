4fa7bae3ce20d8fc3465c581829f92ca0b737054
Jason Grant <jason82autotech@gmail.com>
2023-08-29 21:39:16 -0700

    stuff

---
+++ script.js
@@ -0,0 +1,24 @@
+//rating star buttons. Only the first star is "clicking"
+const ratingButton = document.getElementsByClassName("flex space-x-1")
+const reviewButton = document.getElementsByClassName("mt-2 bg-yellow-500 text-white px-3 py-1 rounded-md")
+
+//const button = document.querySelector('button');
+ratingButton.addEventListener('click', function(){
+    
+}
+);
+
+
+function onButtonClick(ratingButton) {
+
+    console.log('rating Button clicked!');
+  }
+  
+ 
+  //Write a review button
+function onButtonClick(reviewButton) {
+
+    console.log('review Button clicked!');
+  }
+  
+
