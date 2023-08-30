b2f40d7528807cd149ab85ee5c248da665b53584
Alexis Lopez <lopez111alex@gmail.com>
2023-08-29 21:14:44 -0700

    Updated style.js
    I made it so when the search button is clicked it opens to a different page titled 'movie-rating-page.html'

---
+++ js/style.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuButton = document.getElementById("menu-btn");
+    const menuItems = document.getElementById("menu-items");
+    const searchInput = document.getElementById('searching');
+    const searchButton = document.querySelector(".search-btn");
+
+    menuButton.addEventListener("click", function() {
+        menuItems.classList.toggle("active");
+    });
+
+    searchButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        const searchingInput = document.getElementById("searching").value;
+        if (searchingInput.trim() !== "") {
+           window.location.href = `movie-rating-page.html?search=${encodeURIComponent(searchInput)}`;
+       
+        }
+    });
+});
