db91195a08a296c874ea8d7267f1bd4912eeea9f
Jason Grant <jason82autotech@gmail.com>
2023-09-04 17:06:33 -0700

    here we go

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //rating star buttons. Only the first star is "clicking"
 const ratingButton = document.getElementsByClassName("bg-yellow-500 text-white px-2 py-1 rounded-md")
 //const reviewButton = document.getElementsByClassName("mt-2 bg-yellow-500 text-white px-3 py-1 rounded-md")
