03c810dd62bb7671b44c598fc4c304f69bab485b
Alexis Lopez <lopez111alex@gmail.com>
2023-09-07 21:33:32 -0700

    Worked on saving title to local storage when submit button is clicked

--- js/movie-rating-page.js
+++ js/movie-rating-page.js
@@ -1,11 +1,12 @@
-const movieResults = document.getElementById('movie-results');
-const movieTemplate = document.getElementById('movie-template');
-const searchParams = new URLSearchParams(window.location.search);
-const searchTerm = searchParams.get('search');
+var movieResults = document.getElementById('movie-results');
+var movieTemplate = document.getElementById('movie-template');
+var searchParams = new URLSearchParams(window.location.search);
+var searchTerm = searchParams.get('search');
+
 if (searchTerm !== '') {
   fetch(`https://imdb8.p.rapidapi.com/auto-complete?q=${searchTerm}`, {
-    "method": "GET",
-    "headers": {
+    method: "GET",
+    headers: {
       'X-RapidAPI-Host': 'imdb8.p.rapidapi.com',
       'X-RapidAPI-Key': '0578e63d4amshee76984ae253556p1fb441jsn418e7dce4027'
     }
@@ -14,78 +15,69 @@ if (searchTerm !== '') {
   .then(data => {
     console.log(data);
     var list = data.d;
-     list.forEach((item) => {
+
+    list.forEach((item) => {
       var name = item.l;
       var poster = item.i.imageUrl;
       var releaseDate = item.y;
-      const movieBox = document.importNode(movieTemplate.content, true);
-      const img = movieBox.querySelector('img');
-      const title = movieBox.querySelector('h2');
-      const releaseYear = movieBox.querySelector('p');
+
+      var movieBox = document.importNode(movieTemplate.content, true);
+      var img = movieBox.querySelector('img');
+      var title = movieBox.querySelector('h2');
+      var releaseYear = movieBox.querySelector('p');
+
       img.src = poster;
       img.alt = name;
       title.textContent = name;
       releaseYear.textContent = `Release Year: ${releaseDate}`;
-       
+
       movieResults.appendChild(movieBox);
     });
   })
-  .catch(err => {
-    console.error(err);
-  });
 }
+
 document.addEventListener("DOMContentLoaded", function() {
-  const searchInput = document.getElementById('searching');
-  const searchButton = document.querySelector(".search-btn");
+  var searchInput = document.getElementById('searching');
+  var searchButton = document.querySelector(".search-btn");
+
   searchButton.addEventListener("click", function(event) {
     event.preventDefault();
-    const searchingInput = searchInput.value;
+    var searchingInput = searchInput.value;
     if (searchingInput.trim() !== "") {
-       window.location.href = `movie-rating-page.html?search=${encodeURIComponent(searchingInput)}`;
+      window.location.href = `movie-rating-page.html?search=${encodeURIComponent(searchingInput)}`;
     }
-  })
   });
+});
+
 document.addEventListener("DOMContentLoaded", function() {
   movieResults.addEventListener("click", function(event) {
     if (event.target.id === "submit-review") {
       event.preventDefault();
-      const form = event.target.closest("form");
+      var form = event.target.closest("form");
       if (form) {
-        const reviewTextarea = form.querySelector("#review");
-
-
-    
+        var reviewTextarea = form.querySelector("#review");
+       
 
         if (reviewTextarea) {
-
-          const userReview = {
-            reviewTextarea: reviewTextarea.value
+          var userReview = {
+            reviewTextarea: reviewTextarea.value,
+            
           };
-
 
           if (userReview.reviewTextarea !== null && userReview.reviewTextarea.trim() !== '') {
             localStorage.setItem('userReview', JSON.stringify(userReview));
-
-        
-          
-  
-          } 
-        } 
-
+          }
+        }
       }
     }
   });
 });
 
-
-
-
-
 document.addEventListener("DOMContentLoaded", function() {
   movieResults.addEventListener("click", function(event) {
     if (event.target.classList.contains("rating-star")) {
-      const clickedStar = event.target;
-      const starButtons = clickedStar.parentElement.querySelectorAll(".rating-star");
+      var clickedStar = event.target;
+      var starButtons = clickedStar.parentElement.querySelectorAll(".rating-star");
 
       starButtons.forEach(function(starButton) {
         starButton.classList.remove("starChecked");
@@ -95,12 +87,3 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 });
-
-
-
-
-
-
-
-
-
