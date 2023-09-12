62d5ff3c51b836a48f5203ddd94bdce42c89784c
Jesse Warwick <jkeller647@gmail.com>
2023-09-11 19:56:37 -0700

    small edits

--- js/style.js
+++ js/style.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const menuButton = document.getElementById("menu-btn");
-    const menuItems = document.getElementById("menu-items");
     const searchInput = document.getElementById('searching');
     const searchButton = document.getElementById("search-btn");
     console.log("DOMContentLoaded event triggered");
@@ -24,15 +22,4 @@ document.addEventListener("DOMContentLoaded", function() {
       console.log("Search button clicked");
       performSearch() (); // Call the search function when the button is clicked
     });
-  });
-  const menuButton = document.getElementById("menu-btn");
-  const menuItems = document.getElementById("menu-items");
-  const searchInput = document.getElementById('searching');
-  const searchButton = document.querySelector(".search-btn");
-  searchButton.addEventListener("click", function(event) {
-    event.preventDefault();
-    const searchingInput = searchInput.value;
-    if (searchingInput.trim() !== "") {
-       window.location.href = `movie-rating-page.html?search=${encodeURIComponent(searchingInput)}`;
-    }
   });
